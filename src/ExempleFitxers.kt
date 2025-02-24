56de5f106378013524e6282a5ee6e37f89ad8ea9
raimonizard <raimon.izard@gmail.com>
2025-02-24 19:30:09 +0100

    feat: afegir exemple fitxers bàsics txt

--- src/ExempleFitxers.kt
+++ src/ExempleFitxers.kt
@@ -1,24 +1,32 @@
 
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.IOException
+import controllers.*
+import utils.readBoolean
+import utils.readWord
 
 fun main() {
-    try {
-        val file = File("./txt/arxiu.txt")
+    var continuar: Boolean = false
 
-        println("Contingut inicial:")
-        file.forEachLine { println(it) }
+    do {
+        var ruta: String = consultarRuta()
+        var existeix: Boolean = comprovarRuta(ruta)
 
-        file.appendText("hola que tal\n")
+        if (existeix) {
+            var esDirectori: Boolean = comprovarDirectori(ruta)
+            if (esDirectori) {
+                println("La ruta especificada apunta a un directori")
+                println("A continuació pots veure el seu contingut:")
+                llistarContingut(ruta)
+            } else {
+                println("La ruta especificada apunta a un fitxer.")
+                println("A continuació pots veure el seu contingut:")
+                llegirFitxer(ruta)
+                escriureFitxer(ruta)
+            }
+        } else {
+            println("ERROR: El fitxer o ruta especificat, no existeix!")
+        }
 
-        println("Contingut final:")
-        file.forEachLine { println(it) }
-    }catch (fnfe: FileNotFoundException) {
-        print(fnfe.message)
-    }catch (ioe: IOException){
-        print(ioe.message)
-    }catch(e: Exception){
-        print("ERROR: Fatal ERROR. Error no tractat")
-    }
+        continuar = readBoolean("Vols tornar al menú principal? (true/false)",
+                    "Opció no acceptada")
+    }while (continuar)
 }
--- src/exception/AuditoriaException.kt
+++
@@ -1,6 +0,0 @@
-package exception
-
-class AuditoriaException: Exception {
-    constructor(): this("Seràs auditat/da! Prepara't!!!") { }
-    constructor(missatge: String): super(missatge) { }
-}
